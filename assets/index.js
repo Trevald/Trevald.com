24cca7c3409f88edebe7e2e5aa451eb91d015c8e
Benjamin Holfve <benjamin@trevald.com>
2026-02-11 21:22:52 +0100

    Better theme switcher

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,23 @@
+const THEMES = ["light", "dark"]
+
 function initThemeSwitcher() {
-    const themeSwitch = document.querySelector(".theme-switch");
-    const radios = themeSwitch.querySelectorAll("input[type='radio']");
+    const themeSwitch = document.querySelector(".theme-switch")
+    const radios = themeSwitch.querySelectorAll("input[type='radio']")
 
     for (var i = 0; i < radios.length; ++i) {
-        radios[i].addEventListener("click", teamSwticherChange);
+        radios[i].addEventListener("click", themeSwitcherChange)
     }
 }
 
-function teamSwticherChange(event) {
-    document.documentElement.dataset.theme = event.target.value;
+function themeSwitcherChange(event) {
+    if (document.documentElement.dataset.theme === event.target.value) {
+        document.documentElement.dataset.theme = THEMES.find((theme) => theme !== event.target.value)
+        const input = document.querySelector(`.theme-switch input[value="${document.documentElement.dataset.theme}"]`)
+        input.checked = true
+        return
+    }
+
+    document.documentElement.dataset.theme = event.target.value
 }
 
-initThemeSwitcher();
+initThemeSwitcher()
